e7c97e2849ed5f575be83909faf2458c24a1c719
xasirmo05-droid <xasirmo05@gmail.com>
2026-01-21 18:16:48 +0100

    funcionalidad en web

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/ddi4_pletina/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
